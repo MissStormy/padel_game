e9ce9b660a6b3bc70d35402e79ac186bd4ae7f28
Lara RG <laragoblin@outlook.com>
2023-10-17 12:35:12 +0200

    Diseño de vistas

--- lib/modelos/vistas.dart
+++ lib/modelos/vistas.dart
@@ -1,1 +1,38 @@
 import 'dart:io';
+import 'package:padel_game/modelos/partido.dart';
+
+class Vistas{
+  //Instancias
+  Partido partido = new Partido();
+
+  //Variables
+  int MGopc = 0; //Menu general
+  int MCopc = 0; //Menu opciones
+  int MPopc = 0; //Menu partido
+
+  //Constructor
+  Vistas(){}
+
+  //Metodos
+  void printMenuGeneral(){
+    print("=============================");
+  }
+  void printMenuConfig(){
+
+  }
+  void printMenuPartido(){
+
+  }
+  void printMarcador(){
+    print("**************************************");
+    print("*       |★| CLUB PADEL R'LYEH        *");
+    print("*                                    *");
+    print("*  Jugadores               SETS      *");
+    print("*                                    *");
+    print("*                                    *");
+    print("*                                    *");
+    print("**************************************");
+
+  }
+
+}
